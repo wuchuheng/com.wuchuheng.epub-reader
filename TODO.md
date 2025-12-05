ef1017399dee88f76c2cdebc17bb1f079b18c588
wuchuheng <root@wuchuheng.com>
2025-12-06 01:50:01 +0800

    docs: add both features into TODO.md

---
+++ TODO.md
@@ -0,0 +1,4 @@
+# Feature
+
+- [ ] Add I18n support
+- [ ] Rename app name from 'epub-reader' to '小白阅读'
--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-c28420ab'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.qst0nj2i5tg"
+    "revision": "0.ih53f1trfio"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
